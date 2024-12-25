b2bcb07486fbb11808333c0f9034eabea3c84ebd
Artkungloveyouna27 <66040233104@udru.ac.th>
2024-12-25 10:52:33 +0700

    update

---
+++ readtxt.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .mytxt{color: #FF6969;}
+    </style>
+</head>
+<body>
+    <h1>ข้อมูลนักศึกษา ใช้ fread</h1>
+<?php
+    $myfile = fopen("myname.txt", "r") or die("Unable to open file!");
+    echo fread($myfile,filesize("myname.txt"));
+    fclose($myfile);
+    echo"<p>-----------------------------------</p>";
+    $myfile = fopen("myname.txt", "r") or die("Unable to open file!");
+    echo fread($myfile,filesize("myname.txt"));
+    fclose($myfile);
+?>
+<h1>ข้อมูลนักศึกษา ใช้ fgets</h1>
+<?php
+    
+    $myfile = fopen("myname.txt", "r") or die("Unable to open file!");
+    echo fgets($myfile);
+    fclose($myfile);
+    
+?>
+<h1>ข้อมูลนักศึกษา ใช้ fgets ร่ววมกับ feof</h1>
+<?php
+$myfile = fopen("myname.txt", "r") or die("Unable to open file!");
+// Output one line until end-of-file
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
+fclose($myfile);
+?>
+</body>
+</html>
